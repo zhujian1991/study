99a6b647f6bf1e30dd099283278c778a7db2f8dc
zhuy001 <zhuy001@galanz.com>
2020-03-03 20:24:58 +0800

    提交:增加swagger2

--- src/main/java/cn/zhu/test/controller/HelloController.java
+++ src/main/java/cn/zhu/test/controller/HelloController.java
@@ -1,16 +1,20 @@
 package cn.zhu.test.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("这是第一个swagger2")
 public class HelloController {
     @RequestMapping("/hell1o")
     @ResponseBody
+    @ApiOperation(value = "程序老仁",notes = "swagger")
     public String index(Model model) {
-       // model.addAttribute("hello", "你好");
         return "hello，程序老仁";
     }
 
--- src/main/java/cn/zhu/test/controller/JpaController.java
+++ src/main/java/cn/zhu/test/controller/JpaController.java
@@ -3,6 +3,8 @@ import cn.zhu.test.dao.AnimalDao;
 import cn.zhu.test.entity.Animal;
 import cn.zhu.test.entity.StudentEntity;
 import cn.zhu.test.service.JpaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/animal")
+@Api
 public class JpaController {
 
     @Autowired
@@ -19,6 +22,7 @@ public class JpaController {
     /**
      * 测试专用，获取animal对象
      */
+    @ApiOperation(value = "获得动物对象",notes = "得到动物表中的json对象")
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public Animal getAnimal(@PathVariable("id") int id){
         return  jpaService.getAnimalById(id);
